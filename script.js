a265373b97c5bb4a06ec5121ef89b1865b56c33f
TheSonchan <sonja.grund@icloud.com>
2025-01-03 15:53:10 +0100

    set up variables for all necessary exchange of data and added day array

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
+let submitForm = document.querySelector("#city-form");
+submitForm.addEventListener("submit", submitButton);
+
 function submitButton(event) {
   let h1 = document.querySelector("h1");
   let cityName = document.querySelector("#city-input");
   event.preventDefault();
   h1.innerHTML = cityName.value;
+  let apiKey = "4d6d3a603f2o058afbtc1e8fa6515357";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityName.value}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(returnTemp);
+  function returnTemp(value) {
+    let currentTemp = Math.round(value.data.temperature.current);
+    let city = value.data.city;
+    let timestamp = value.data.time;
+    let description = value.data.condition.description;
+    let humidity = value.data.temperature.humidity;
+    let wind = value.data.wind.speed;
+    console.log(wind);
+  }
 }
-let submitForm = document.querySelector("#city-form");
-submitForm.addEventListener("submit", submitButton);
+function formatDate() {
+  days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+}
